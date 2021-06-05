1f3381bfe43ce9dee1f370957b9e66432c9c13ea
MichaelGeneRuiz <michael.gene.ruiz@gmail.com>
2021-06-05 00:46:56 -0400

    About Section

--- scripts.js
+++ scripts.js
@@ -1,5 +1,6 @@
-function hamburger(){
-    if(document.getElementById("navi-toggle").checked == true){
-        document.getElementById("navi-toggle").checked = false;
-    }
+// Function to close the hamburger screen when it is open
+function hamburger() {
+  if (document.getElementById("navi-toggle").checked == true) {
+    document.getElementById("navi-toggle").checked = false;
+  }
 }
